e02f94d5e61f7ef23a796bdb44c934549b2396df
Johann-code <sebastian854as@gmail.com>
2025-02-18 22:47:04 -0500

    planes de salud

--- catalogo.js
+++ catalogo.js
@@ -3,15 +3,109 @@ function usuario() {
 }
 usuario();
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     loadCatalog();
 });
 
 const plans = [
-    { name: "Plan Básico", price: "$20/mes", description: "Cobertura médica básica." },
-    { name: "Plan Avanzado", price: "$50/mes", description: "Cobertura completa y más beneficios." },
-    { name: "Plan Premium", price: "$80/mes", description: "Atención VIP y servicios exclusivos." }
+    {
+        category: "Joven aulto mayor",
+        options: [
+            {
+                name: "Plan Vital",
+                price: "$15/mes",
+                description: [
+                    "Cobertura para servicios preventivos como chequeos médicos y vacunas." ,
+                    "Acceso a programas de ejercicio y actividad física.",
+                    "Descuento para servicios de salud mental y bienestar",
+                ]
+            },
+            {
+                name: "Plan Activo",
+                price: "$35/mes",
+                description: [
+                    "Todo incluido en el plan Vital.",
+                    "Cobertura para servicios de rehabilitación y terapia física.",
+                    "Acceso a programas de manejo del estrés y la ansiedad.",
+                ]
+            },
+            { 
+                name: "Plan Premium", 
+                price: "$60/mes", 
+                description: [
+                    "Todo lo incluido en los Planes Vital y Activo.",
+                    "Cobertura para servicios de medicina alternativa y complementaria.",
+                    "Acceso a programas de viaje y aventura para adultos mayores.",
+                ]
+            }
+        ]
+    },
+    {
+        category: "Adulto mayor establecido",
+        options: [
+            { 
+                name: "Plan Seguro", 
+                price: "$20/mes", 
+                description: [
+                    "Cobertura para servicios de atención médica primaria y especializada.",
+                    "Acceso a programas de manejo de enfermedades crónicas.",
+                    "Descuentos en servicios de salud en el hogar.",
+                ] 
+            },
+            { 
+                name: "Plan Protegido", 
+                price: "$50/mes", 
+                description: [
+                    "Todo lo incluido en el Plan Seguro.",
+                    "Cobertura para servicios de terapia ocupacional y de habla.",
+                    "Acceso a programas de apoyo para cuidadores.",
+                ]
+            },
+            { 
+                name: "Plan Excelente", 
+                price: "$80/mes", 
+                description: [
+                    "Todo lo incluido en los Planes Seguro y Protegido.",
+                    "Cobertura para servicios de medicina paliativa y cuidados al final de la vida.",
+                    "Acceso a programas de viaje y aventura para adultos mayores con necesidades especiales.",
+                ]
+            }
+        ]
+    },
+    {
+        category: "Adulto mayor avanzado",
+        options: [
+            { 
+                name: "Plan Cuidado", 
+                price: "$30/mes", 
+                description: [
+                    "Cobertura para servicios de atención médica en el hogar.",
+                    "Acceso a programas de manejo de enfermedades crónicas y complejas.",
+                    "Descuentos en servicios de salud en el hogar.",
+                ] 
+            },
+            { 
+                name: "Plan Apoyo", 
+                price: "$60/mes", 
+                description: [
+                    "Todo lo incluido en el Plan Cuidado.",
+                    "Cobertura para servicios de terapia física, ocupacional y de habla.",
+                    "Acceso a programas de apoyo para cuidadores y familiares.",
+                ]
+            },
+            { 
+                name: "Plan Integrado", 
+                price: "$100/mes", 
+                description: [
+                    "Todo lo incluido en los Planes Cuidado y Apoyo.",
+                    "Cobertura para servicios de medicina paliativa y cuidados al final de la vida.",
+                    "Acceso a programas de viaje y aventura para adultos mayores con necesidades especiales y complejas.",
+                ]
+            }
+        ]
+    }
 ];
+
 
 function loadCatalog() {
     const catalog = document.getElementById("catalog");
@@ -28,7 +122,7 @@ function openModal(name, price) {
     // alert("quiero_dormir")
     document.getElementById("purchaseModal").classList.add("modal--show");
     document.getElementById("selectedPlan").innerHTML = `Plan seleccionado: ${name} - ${price}`;
-    
+
 }
 
 function closeModal() {
